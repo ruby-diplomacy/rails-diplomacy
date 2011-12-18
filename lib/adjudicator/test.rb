e4ec44985bf684d0232836d05925d61fdc0f14a8
Nameless <unfortunate42@gmail.com>
2011-12-19 01:46:40 +0200

    Add convoy test.

--- lib/adjudicator/test.rb
+++ lib/adjudicator/test.rb
@@ -38,13 +38,40 @@ class TestAdjudication < Test::Unit::TestCase
     
   end
   
+  def test_convoy
+    units = [Diplomacy::Unit.new(:Turkey, Diplomacy::Unit::FLEET)]*2 + 
+      [Diplomacy::Unit.new(:Russia, Diplomacy::Unit::ARMY)] + 
+      [Diplomacy::Unit.new(:Russia, Diplomacy::Unit::FLEET)]
+    
+    @gs[:Sev] = Diplomacy::AreaState.new(:Russia, units[2])
+    @gs[:Bla] = Diplomacy::AreaState.new(:Russia, units[3])
+    
+    @gs[:Bul] = Diplomacy::AreaState.new(:Turkey, units[0])
+    @gs[:Con] = Diplomacy::AreaState.new(:Turkey, units[1])
+    @gs[:Ank] = Diplomacy::AreaState.new(:Turkey, nil)
+    
+    (orders = []) << Diplomacy::Move.new(units[2], :Sev, :Ank) <<
+      Diplomacy::Convoy.new(units[3], :Bla, :Sev, :Ank) <<
+      Diplomacy::Support.new(units[1], :Con, :Bul, :Bla) <<
+      Diplomacy::Move.new(units[0], :Bul, :Bla)
+    
+    state_after,orders = @engine.resolve(@gs, orders)
+    
+    orders.each do |order|
+      puts order.to_s
+    end
+    
+    assert_equal(Diplomacy::OrderWrapper::FAILURE, orders[1].status, "#{orders[1].to_s} should have been cut by #{orders[3]}")
+    assert_equal(Diplomacy::OrderWrapper::FAILURE, orders[0].status, "#{orders[0].to_s} should have failed because #{orders[3].to_s} cuts #{orders[1]}")
+  end
+  
   def test_simple
     
-    russian_units = [Diplomacy::Unit.new(:Russia, Diplomacy::Unit::ARMY), Diplomacy::Unit.new(:Russia, Diplomacy::Unit::ARMY)]
+    russian_units = [Diplomacy::Unit.new(:Russia, Diplomacy::Unit::ARMY)]*2
     @gs[:Ukr] = Diplomacy::AreaState.new(:Russia, russian_units[0])
     @gs[:Mos] = Diplomacy::AreaState.new(:Russia, russian_units[1])
     
-    german_units = [Diplomacy::Unit.new(:Germany, Diplomacy::Unit::ARMY), Diplomacy::Unit.new(:Germany, Diplomacy::Unit::ARMY)]
+    german_units = [Diplomacy::Unit.new(:Germany, Diplomacy::Unit::ARMY)]*2
     @gs[:War] = Diplomacy::AreaState.new(:Russia, german_units[0])
     @gs[:Gal] = Diplomacy::AreaState.new(:Russia, german_units[1])
     

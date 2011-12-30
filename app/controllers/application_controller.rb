59e0d2a4c6e17efa0397906c684aad1495175793
George Avramidis <gavramidis@skroutz.gr>
2011-12-30 15:54:06 +0200

    MessagesController Specs, User Specs. Some Authentication in messages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,26 @@ require 'dm-rails/middleware/identity_map'
 
 class ApplicationController < ActionController::Base
   use Rails::DataMapper::Middleware::IdentityMap
+
+  rescue_from DataMapper::ObjectNotFoundError, :with => :record_not_found
+  rescue_from User::NotAuthorizedError, :with  => :user_not_authorized
   protect_from_forgery
 
-
+  # FIXME: stub
+  def require_login
+    @user = logged_user
+  end
+  
+  # FIXME: authentication
   def logged_user
     User.first
+  end
+
+  def record_not_found
+    raise ActionController::RoutingError.new('Not Found!')
+  end
+
+  def user_not_authorized
+    render :file => 'public/401.html', :status => :unauthorized
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,56 +1,67 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-require 'capybara/rails'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
 
 
-Dir[Rails.root.join("spec/factories/**/*.rb")].each {|f| require f}
+
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'capybara/rails'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
+  Dir[Rails.root.join("spec/factories/**/*.rb")].each {|f| require f}
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
+  RSpec.configure do |config|
+    # == Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+    config.mock_with :rspec
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.before(:suite) do 
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    # config.use_transactional_fixtures = true
+
+    # If true, the base class of anonymous controllers will be inferred
+    # automatically. This will be the default behavior in future versions of
+    # rspec-rails.
+    config.infer_base_class_for_anonymous_controllers = false
+
+    config.before(:suite) do 
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+
+
+
   end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-  
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-
-
 end

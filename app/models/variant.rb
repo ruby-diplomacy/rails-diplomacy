45ca8f566aad157c1728ddadde75b7d9e26b2cd5
George Avramidis <avramidg@gmail.com>
2012-04-14 16:38:41 +0300

    added Variant#options_for_select

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -2,6 +2,13 @@ class Variant < ActiveRecord::Base
 
   has_many :games
   has_many :powers
+
+
+  def self.options_for_select
+    Variant.select([:id, :name]).collect do |v|
+      [v.name, v.id]
+    end
+  end
 end
 # == Schema Information
 #

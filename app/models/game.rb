d34cc11688a88ea9e8e72c06b5417d8ed22393ef
NamelessOne <unfortunate42@gmail.com>
2013-06-18 02:24:01 +0200

    Added rudimentary state dumping and parsing.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,19 @@
 class Game < ActiveRecord::Base
   has_many :power_assignments
   has_many :users, through: :power_assignments
-  has_many :state
+  has_many :states
 
-  attr_accessible :name, :power_assignments_attributes, :state
+  attr_accessible :name, :power_assignments_attributes, :states
   
   accepts_nested_attributes_for :power_assignments, reject_if: proc { |a| a[:power].blank? }, :allow_destroy => true
 
+  after_create :create_initial_state
+
   def current_state
+    states.order("turn DESC").first
+  end
+
+  def create_initial_state
+    states.create turn: 1, positions: "Placeholder"
   end
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,9 @@
 class State < ActiveRecord::Base
   belongs_to :game
   attr_accessible :positions, :turn
+
+  def to_gamestate
+    state_parser = Diplomacy::StateParser.new
+    state_parser.parse_units positions || ""
+  end
 end

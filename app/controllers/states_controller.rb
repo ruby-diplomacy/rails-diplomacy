9d13d1da180ed425469879af0c1189bf875a32f6
NamelessOne <unfortunate42@gmail.com>
2013-06-23 23:18:43 +0200

    Fix for previous commit (current state broke).

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,8 +2,13 @@ class StatesController < ApplicationController
   respond_to :json
 
   def show
-    @game = Game.find(params[:game_id])
-    @state = @game.states.find_by_turn(params[:id])
+    if params.has_key? :game_id
+      @game = Game.find(params[:game_id])
+      @state = @game.states.find_by_turn(params[:id])
+    else
+      @game = Game.find(params[:id])
+      @state = @game.current_state
+    end
 
     @state.state = Diplomacy::StateParser.new.parse_state @state.state
 

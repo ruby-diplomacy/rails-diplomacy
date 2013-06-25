b08b5da26677ecfb1a9ff777bf9dec857d381668
NamelessOne <unfortunate42@gmail.com>
2013-06-25 19:25:59 +0200

    Added devise.

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ end
 
 gem 'ruby-adjudicator'
 
+gem 'devise'
+
 # To use ActiveModel has_secure_password
 
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ RailsDiplomacy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root to: 'games#index'
 
   # See how all your routes lay out with "rake routes"
 
